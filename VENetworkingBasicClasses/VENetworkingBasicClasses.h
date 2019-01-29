313bf4ad22db8ca9faca8a4bbc60134724b7e156
vinod k <vinod@vinods-mac-mini.local>
2019-01-29 10:55:12 +0530

    Initial Commit

---
+++ VENetworkingBasicClasses/VENetworkingBasicClasses.h
@@ -0,0 +1,19 @@
+//
+//  VENetworkingBasicClasses.h
+//  VENetworkingBasicClasses
+//
+//  Created by vinod k on 29/01/19.
+//  Copyright © 2019 tvisha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VENetworkingBasicClasses.
+FOUNDATION_EXPORT double VENetworkingBasicClassesVersionNumber;
+
+//! Project version string for VENetworkingBasicClasses.
+FOUNDATION_EXPORT const unsigned char VENetworkingBasicClassesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VENetworkingBasicClasses/PublicHeader.h>
+
+
